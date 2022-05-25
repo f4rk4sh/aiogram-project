f680b5cdfbf2953180e1a78c62cccdd56d19cd77
f4rk4sh <farkashyaroslav@gmail.com>
2022-05-25 19:03:50 +0300

    setting up redis for state storing, fixing dockerfile, upfating requirements.txt, .env.example

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -8,6 +8,7 @@ from data.config import ADMINS
 async def on_startup_notify(dp: Dispatcher):
     for admin in ADMINS:
         try:
-            await dp.bot.send_message(chat_id=admin, text='Bot is up and running')
+            await dp.bot.send_message(chat_id=admin, text='<b>Notification:</b>\n\n'
+                                                          'Bot is up and running')
         except Exception as e:
             logging.exception(e)
